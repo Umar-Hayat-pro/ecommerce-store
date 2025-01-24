593a18c3fafb9889e9e98128354c008450161668
Umar-Hayat-pro <umarhayatlohar0000@gmail.com>
2025-01-24 14:52:13 +0500

    Working on stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "up" => "rails/health#show", as: :rails_health_check
 
 
-
+  get "store" => "store#index"
   get "about" => "home#about"
   get "contact" => "home#contact"
   root "home#index"
