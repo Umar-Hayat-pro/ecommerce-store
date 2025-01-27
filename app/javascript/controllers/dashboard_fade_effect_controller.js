c2e6996ff9298154521a3b93bfb601723daec8a0
Umar-Hayat-pro <umarhayatlohar0000@gmail.com>
2025-01-27 11:42:12 +0500

    Making more changes i hope this idea works

---
+++ app/javascript/controllers/dashboard_fade_effect_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  connect() {
+    this.element.classList.add("fade-in");
+  }
+
+  disconnect() {
+    this.element.classList.add("fade-out");
+  }
+}
