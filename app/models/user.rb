1c998a982650fc2aa9e6e361351e56ba9fcd21b4
Umar-Hayat-pro <umarhayatlohar0000@gmail.com>
2025-01-27 13:43:05 +0500

    minor changes now looking into user roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,6 @@
 class User < ApplicationRecord
-  before_save :set_default_role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  #  enum role: { user: 0, product_manager: 1, order_manager: 2, accounts_manager: 3, super_admin: 4 }
-
-  # validates :role, presence: true
-
-  # private
-  # def set_default_role
-  #   self.role ||= :user
-  # end
 end
